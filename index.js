f2276ebd893f21f658a07567ea1e7d52ff694047
Tuyen Ha Bocasay <tuyenha@bocasay.com>
2019-01-17 20:28:09 +0700

    adding influx db

--- index.js
+++ index.js
@@ -1,16 +1,82 @@
 'use strict';
 
 const express = require('express');
-
+const Influx = require('influx');
+const os = require('os');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const influx = new Influx.InfluxDB({
+  host: HOST,
+  database: 'sensordata',
+  schema: [
+    {
+      measurement: 'response_times',
+      fields: {
+        path: Influx.FieldType.STRING,
+        duration: Influx.FieldType.INTEGER
+      },
+      tags: [
+        'host'
+      ]
+    }
+  ]
+})
+
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.use((req, res, next) => {
+  const start = Date.now()
+
+  res.on('finish', () => {
+    const duration = Date.now() - start
+    console.log(`Request to ${req.path} took ${duration}ms`);
+
+    influx.writePoints([
+      {
+        measurement: 'response_times',
+        tags: { host: os.hostname() },
+        fields: { duration, path: req.path },
+      }
+    ]).catch(err => {
+      console.error(`Error saving data to InfluxDB! ${err.stack}`)
+    })
+  })
+  return next()
+})
+
+app.get('/', function (req, res) {
+  setTimeout(() => res.end('Hello world!'), Math.random() * 500)
+})
+
+app.get('/times', function (req, res) {
+  influx.query(`
+    select * from response_times
+    where host = ${Influx.escape.stringLit(os.hostname())}
+    order by time desc
+    limit 10
+  `).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).send(err.stack)
+  })
+})
+
+
+influx.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('express_response_db')) {
+      return influx.createDatabase('express_response_db');
+    }
+  })
+  .then(() => {
+    app.listen(PORT, HOST);
+    console.log(`Running on http://${HOST}:${PORT}`);
+  })
+  .catch(err => {
+    console.error(`Error creating Influx database!`);
+  })
+
